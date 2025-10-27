4010eeeec43d87756e94f1fecb26fe0572180ece
Trestan Simon <trestansimon@proton.me>
2025-10-27 17:00:53 -0600

    Add website files

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    new DataTable('#sadcat-table', {
+        ajax: '../data/sadcat.json',
+        layout: {
+            topStart: {buttons: ['copy', 'csv'],},
+        },
+        info: false,
+        ordering: false,
+        paging: false,
+        columnDefs: [
+            {
+                targets: '_all',
+                className: 'dt-right',
+            },
+            {
+                targets: [0,1,2],
+                render: DataTable.render.datetime('YYYY-MM-DD hh:mm'),
+            }
+        ],
+    });
+});
