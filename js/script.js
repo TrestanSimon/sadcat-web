c17c9584877d744ed7b86eb349c5021dce4331b4
Trestan Simon <trestansimon@proton.me>
2025-10-31 03:21:15 -0600

    Update table and enable ordering

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     new DataTable('#sadcat-table', {
-        ajax: '/sadcat-web/data/sadcat.json',
+        ajax: '../data/sadcat.json',
         layout: {
             topStart: {buttons: ['copy', 'csv'],},
         },
         info: false,
-        ordering: false,
+        ordering: true,
         paging: false,
         columnDefs: [
             {
@@ -13,8 +13,42 @@ $(document).ready(function(){
                 className: 'dt-right',
             },
             {
-                targets: [0,1,2],
+                targets: [1, 2, 3],
                 render: DataTable.render.datetime('YYYY-MM-DD hh:mm'),
+            },
+            {
+                targets: 6,
+                render: function (data, type) {
+                    if (type === 'sort' || type === 'type') {
+                        return data
+                    } else {
+                        prefix = parseInt(data[6])
+                        suffix = data.slice(0, 3)
+                        if (prefix == 6) {
+                            return 'C' + suffix
+                        } else if (prefix == 5) {
+                            return 'M' + suffix
+                        } else if (prefix == 4) {
+                            return 'X' + suffix
+                        } else if (prefix == 3) {
+                            return 'X' + data[0] + data[2]
+                        } else { return data }
+                    }
+                }
+            },
+            {
+                targets: 12,
+                render: function (data, type) {
+                    if (type === 'sort' || type === 'type') {
+                        if (data == 'N/A') {
+                            return 0
+                        } else {
+                            return data
+                        }
+                    } else {
+                        return data
+                    }
+                }
             }
         ],
     });
